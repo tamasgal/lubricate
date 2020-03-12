fdbcbfcfe3d0746175f8151f197fe753592712ba
Tamas Gal <tgal@km3net.de>
2020-03-12 19:13:04 +0100

    Restructure

--- lubricate/cmd.py
+++ lubricate/cmd.py
@@ -41,6 +41,19 @@ def create_folder_structure(path):
     shutil.copytree(TEMPLATE_FOLDER, path)
 
 
+def merge_folders(source, destination):
+    for src_dir, dirs, files in os.walk(source):
+        dst_dir = src_dir.replace(source, destination, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.copy(src_file, dst_dir)
+
+
 def initialise_git(path):
     """Initialise a Git repository"""
     base_cmd = ["git", "-C", path]
